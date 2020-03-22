47ec32ca38576f5799a06997912df95e3038c523
Kim LaRocca <kim@kimlarocca.com>
2020-03-22 11:12:36 -0400

    mysqli updates

--- Connections/cms.php
+++ Connections/cms.php
@@ -8,6 +8,6 @@ $hostname_cms = "localhost";
 $database_cms = "kim_4site";
 $username_cms = "kim_larocca";
 $password_cms = "Lotus18641864!";
-$cms = mysqli_connect($hostname_cms, $username_cms, $password_cms) or trigger_error(mysqli_error(),E_USER_ERROR); 
+$cms = mysqli_connect($hostname_cms, $username_cms, $password_cms, $database_cms) or trigger_error(mysqli_error(),E_USER_ERROR);
 $websiteID = 9;
 ?>
